40592b42530a0596c5f86e96d278c20750cd5ce3
LDY <leedy3838@naver.com>
2022-06-29 14:33:55 +0900

    EditText 활용

--- app/src/main/java/com/example/androidstudioproject/MainActivity.kt
+++ app/src/main/java/com/example/androidstudioproject/MainActivity.kt
@@ -3,14 +3,31 @@ package com.example.androidstudioproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        et_age.setOnEditorActionListener{v, actionId, event ->
+            if(actionId == EditorInfo.IME_ACTION_DONE){
+                login(v)
+                true;
+            }
+            else false;
+        }
     }
-    fun clicked(v : View){
-        Btn1.text = "clicked"
+
+    fun login(v : View){
+        if(et_email.text.toString() == "ldy@naver.com" && et_password.text.toString() == "1234"
+            && et_name.text.toString() == "LDY" && et_age.text.toString() == "23"){
+            Toast.makeText(this, "로그인 되었습니다.", Toast.LENGTH_SHORT).show();
+        }
+        else
+            Toast.makeText(this, "로그인 하지 못했습니다.", Toast.LENGTH_SHORT).show();
     }
 }
+
