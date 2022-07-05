05f00619d02e27c341f2d7e47012abd28a33aa94
LDY <leedy3838@naver.com>
2022-07-05 15:49:21 +0900

    기본 화면 구성

--- app/src/main/java/com/example/androidstudioproject/MainActivity.kt
+++ app/src/main/java/com/example/androidstudioproject/MainActivity.kt
@@ -14,30 +14,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        et_age.setOnEditorActionListener{v, actionId, event ->
-            if(actionId == EditorInfo.IME_ACTION_DONE){
-                login(v)
-                true;
-            }
-            else false;
-        }
-    }
-
-    fun login(v : View){
-        var imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(v.windowToken, 0)
-
-        if(et_name.text.toString() == "LDY" && et_age.text.toString() == "23"){
-            Toast.makeText(this, "사용자 확인되었습니다.", Toast.LENGTH_SHORT).show();
-
-            var intent = Intent(this, SubActivity::class.java)
-            intent.putExtra("이름", "LDY")
-            intent.putExtra("나이", 23)
-
-            startActivity(intent)
-        }
-        else
-            Toast.makeText(this, "로그인 하지 못했습니다.", Toast.LENGTH_SHORT).show();
     }
 }
