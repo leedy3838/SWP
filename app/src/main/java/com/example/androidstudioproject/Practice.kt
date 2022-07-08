a7098e3bed0268f78242a9dc2babc6255a3a7d18
LDY <leedy3838@naver.com>
2022-07-08 15:51:38 +0900

    파이어베이스 이용 사진 가져오기

---
+++ app/src/main/java/com/example/androidstudioproject/Practice.kt
@@ -0,0 +1,21 @@
+package com.example.androidstudioproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import kotlinx.android.synthetic.main.practicedatabase.*
+
+class Practice : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.practicedatabase)
+
+        btn.setOnClickListener {
+            val storage: FirebaseStorage = FirebaseStorage.getInstance()
+            val storageRef: StorageReference = storage.getReference("image.png")
+            Glide.with(btn).load(storageRef).into(imageView2)
+        }
+    }
+}
