52932da34f9e4d7772e0365b405ac7779c4de3fe
LDY <leedy3838@naver.com>
2022-07-13 15:03:41 +0900

    문제 풀이 화면(화면에 선택한 문제가 나오게 설정)

--- app/src/main/java/com/example/androidstudioproject/ProblemSolveScreen.kt
+++ app/src/main/java/com/example/androidstudioproject/ProblemSolveScreen.kt
@@ -4,11 +4,40 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import kotlinx.android.synthetic.main.problem_solve.*
 
 class ProblemSolveScreen :AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.problem_solve)
+
+        val db = FirebaseFirestore.getInstance()
+        var st : String = ""
+
+        val docRef = db.collection("고등학생")
+            .document("3학년")
+            .collection("수학")
+            .document("공통")
+            .collection("공통")
+            .document("2022년 3월 모의고사 10번")
+
+        docRef.get()
+            .addOnSuccessListener { document ->
+                st = document.get("이미지").toString()
+
+                val storage: FirebaseStorage = FirebaseStorage.getInstance()
+                val storageRef: StorageReference = storage.getReference(st)
+
+                Glide.with(this)
+                    .load(storageRef)
+                        //대기 화면(placeholder)
+                    .placeholder(R.drawable.text_background)
+                    .into(imageViewProblemSolve)
+            }
     }
 
     fun home(v : View){
