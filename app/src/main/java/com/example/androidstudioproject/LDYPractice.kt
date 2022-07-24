29eb43b24d73ff8b0585ecc843745e6eac4fce81
LDY <leedy3838@naver.com>
2022-07-24 16:24:50 +0900

    기본 화면 원래 화면으로 변경

--- app/src/main/java/com/example/androidstudioproject/LDYPractice.kt
+++ app/src/main/java/com/example/androidstudioproject/LDYPractice.kt
@@ -17,215 +17,94 @@ class LDYPractice : AppCompatActivity() {
 
         val students = db.collection("고등학생")
             .document("3학년")
-            .collection("사회탐구")
-            .document("생활과 윤리")
-            .collection("생활과 윤리")
+            .collection("수학")
+            .document("확률과 통계")
+            .collection("확률과 통계")
 
 
-        val data1 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 1번.png",
+        val data23 = hashMapOf(
+            "출제 년도" to "2022년 6월 모의고사",
+            "경로" to "/3학년/수학/확률과 통계/2022년 6월 모의고사/2022년 6월 모의고사 23번.png",
             "시도 횟수" to 1,
             "정답수" to 1,
             "정답률" to 100,
             "정답" to 2
         )
-        students.document("2021년 3월 모의고사 1번").set(data1)
+        students.document("2022년 6월 모의고사 23번").set(data23)
 
-        val data2 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 2번.png",
-            "시도 횟수" to 1,
-            "정답수" to 1,
-            "정답률" to 100,
-            "정답" to 5
-        )
-        students.document("2021년 3월 모의고사 2번").set(data2)
-
-        val data3 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 3번.png",
-            "시도 횟수" to 1,
-            "정답수" to 1,
-            "정답률" to 100,
-            "정답" to 5
-        )
-        students.document("2021년 3월 모의고사 3번").set(data3)
-
-        val data4 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 4번.png",
-            "시도 횟수" to 1,
-            "정답수" to 1,
-            "정답률" to 100,
-            "정답" to 4
-        )
-        students.document("2021년 3월 모의고사 4번").set(data4)
-
-        val data5 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 5번.png",
-            "시도 횟수" to 1,
-            "정답수" to 1,
-            "정답률" to 100,
-            "정답" to 4
-        )
-        students.document("2021년 3월 모의고사 5번").set(data5)
-
-        val data6 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 6번.png",
-            "시도 횟수" to 1,
-            "정답수" to 1,
-            "정답률" to 100,
-            "정답" to 3
-        )
-        students.document("2021년 3월 모의고사 6번").set(data6)
-
-        val data7 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 7번.png",
-            "시도 횟수" to 1,
-            "정답수" to 1,
-            "정답률" to 100,
-            "정답" to 4
-        )
-        students.document("2021년 3월 모의고사 7번").set(data7)
-
-        val data8 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 8번.png",
+        val data24 = hashMapOf(
+            "출제 년도" to "2022년 6월 모의고사",
+            "경로" to "/3학년/수학/확률과 통계/2022년 6월 모의고사/2022년 6월 모의고사 24번.png",
             "시도 횟수" to 1,
             "정답수" to 1,
             "정답률" to 100,
             "정답" to 1
         )
-        students.document("2021년 3월 모의고사 8번").set(data8)
-
-        val data9 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 9번.png",
-            "시도 횟수" to 1,
-            "정답수" to 1,
-            "정답률" to 100,
-            "정답" to 1
-        )
-        students.document("2021년 3월 모의고사 9번").set(data9)
-
-        val data10 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 10번.png",
+        students.document("2022년 6월 모의고사 24번").set(data24)
+        val data25 = hashMapOf(
+            "출제 년도" to "2022년 6월 모의고사",
+            "경로" to "/3학년/수학/확률과 통계/2022년 6월 모의고사/2022년 6월 모의고사 25번.png",
             "시도 횟수" to 1,
             "정답수" to 1,
             "정답률" to 100,
             "정답" to 4
         )
-        students.document("2021년 3월 모의고사 10번").set(data10)
+        students.document("2022년 6월 모의고사 25번").set(data25)
 
-        val data11 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 11번.png",
+        val data26 = hashMapOf(
+            "출제 년도" to "2022년 6월 모의고사",
+            "경로" to "/3학년/수학/확률과 통계/2022년 6월 모의고사/2022년 6월 모의고사 26번.png",
             "시도 횟수" to 1,
             "정답수" to 1,
             "정답률" to 100,
             "정답" to 2
         )
-        students.document("2021년 3월 모의고사 11번").set(data11)
+        students.document("2022년 6월 모의고사 26번").set(data26)
 
-        val data12 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 12번.png",
+        val data27 = hashMapOf(
+            "출제 년도" to "2022년 6월 모의고사",
+            "경로" to "/3학년/수학/확률과 통계/2022년 6월 모의고사/2022년 6월 모의고사 27번.png",
+            "시도 횟수" to 1,
+            "정답수" to 1,
+            "정답률" to 100,
+            "정답" to 3
+        )
+        students.document("2022년 6월 모의고사 27번").set(data27)
+
+        val data28 = hashMapOf(
+            "출제 년도" to "2022년 6월 모의고사",
+            "경로" to "/3학년/수학/확률과 통계/2022년 6월 모의고사/2022년 6월 모의고사 28번.png",
             "시도 횟수" to 1,
             "정답수" to 1,
             "정답률" to 100,
             "정답" to 4
         )
-        students.document("2021년 3월 모의고사 12번").set(data12)
+        students.document("2022년 6월 모의고사 28번").set(data28)
 
-        val data13 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 13번.png",
+        val data29 = hashMapOf(
+            "출제 년도" to "2022년 6월 모의고사",
+            "경로" to "/3학년/수학/확률과 통계/2022년 6월 모의고사/2022년 6월 모의고사 29번.png",
             "시도 횟수" to 1,
             "정답수" to 1,
             "정답률" to 100,
-            "정답" to 1
+            "정답" to 115
         )
-        students.document("2021년 3월 모의고사 13번").set(data13)
+        students.document("2022년 6월 모의고사 29번").set(data29)
 
-        val data14 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 14번.png",
+        val data30 = hashMapOf(
+            "출제 년도" to "2022년 6월 모의고사",
+            "경로" to "/3학년/수학/확률과 통계/2022년 6월 모의고사/2022년 6월 모의고사 30번.png",
             "시도 횟수" to 1,
             "정답수" to 1,
             "정답률" to 100,
-            "정답" to 3
+            "정답" to 9
         )
-        students.document("2021년 3월 모의고사 14번").set(data14)
-
-        val data15 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 15번.png",
-            "시도 횟수" to 1,
-            "정답수" to 1,
-            "정답률" to 100,
-            "정답" to 2
-        )
-        students.document("2021년 3월 모의고사 15번").set(data15)
-
-        val data16 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 16번.png",
-            "시도 횟수" to 1,
-            "정답수" to 1,
-            "정답률" to 100,
-            "정답" to 1
-        )
-        students.document("2021년 3월 모의고사 16번").set(data16)
-
-        val data17 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 17번.png",
-            "시도 횟수" to 1,
-            "정답수" to 1,
-            "정답률" to 100,
-            "정답" to 5
-        )
-        students.document("2021년 3월 모의고사 17번").set(data17)
-
-        val data18 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 18번.png",
-            "시도 횟수" to 1,
-            "정답수" to 1,
-            "정답률" to 100,
-            "정답" to 2
-        )
-        students.document("2021년 3월 모의고사 18번").set(data18)
-
-        val data19 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 19번.png",
-            "시도 횟수" to 1,
-            "정답수" to 1,
-            "정답률" to 100,
-            "정답" to 3
-        )
-        students.document("2021년 3월 모의고사 19번").set(data19)
-
-        val data20 = hashMapOf(
-            "출제 년도" to "2021년 3월 모의고사",
-            "경로" to "/3학년/사회탐구/생활과 윤리/2021년 3월 모의고사/2021년 3월 모의고사 20번.png",
-            "시도 횟수" to 1,
-            "정답수" to 1,
-            "정답률" to 100,
-            "정답" to 3
-        )
-        students.document("2021년 3월 모의고사 20번").set(data20)
+        students.document("2022년 6월 모의고사 30번").set(data30)
 
 
         var st = ""
 
-        val docRef = students.document("2021년 3월 모의고사 8번")
+        val docRef = students.document("2022년 6월 모의고사 25번")
 
         docRef.get()
             .addOnSuccessListener { document ->
