6a79b5b78723068b9fa6c93ade54f994b059863d
jongchanim <99734417+chani2104@users.noreply.github.com>
2022-07-27 14:07:08 +0900

    07.27 정답 오답 팝업 및 정답 입력창 조건 추가

--- app/src/main/java/com/example/androidstudioproject/ProblemSolveScreen.kt
+++ app/src/main/java/com/example/androidstudioproject/ProblemSolveScreen.kt
@@ -2,10 +2,14 @@ package com.example.androidstudioproject
 
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.NonCancellable.cancel
@@ -25,6 +29,25 @@ class ProblemSolveScreen :AppCompatActivity() {
         val btn_giveUp = findViewById<Button>(R.id.giveUp)
         //정답 제출 기능
         val btn_answerSubmit = findViewById<Button>(R.id.answerSubmit)
+        //입력한 숫자
+        val textInputEditText = findViewById<EditText>(R.id.textInputEditText)
+
+        btn_answerSubmit.isEnabled = false  // 버튼 초기 상태 비활성화
+
+        textInputEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                //텍스트 입력 후
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //텍스트 입력 전
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //텍스트 입력 중
+                val message = textInputEditText.getText().toString();
+                btn_answerSubmit.isEnabled = !message.isEmpty()  // 정답 입력 칸에 숫자가 생기면 버튼 활성화
+            }
+        })
+
 
         btn_giveUp.setOnClickListener {
             val builder = AlertDialog.Builder(this)
@@ -50,11 +73,11 @@ class ProblemSolveScreen :AppCompatActivity() {
         var answerRate : Int = intent.getIntExtra("정답률", 100)
         val user = intent.getStringExtra("유저")
 
-        var answer : Long
+        var answer : Long = 0    // 정답 번호를 받아오기 전 초기화
         var questionYear : String
         var tryNum : Long
         var answerRateInDocument : Long
-        var answerNum : Long = 0
+        var answerNum : Long
 
         intent = Intent(this, ProblemSolveNextScreen::class.java)
 
@@ -84,6 +107,8 @@ class ProblemSolveScreen :AppCompatActivity() {
                             intent.putExtra("과목", subject)
                             intent.putExtra("유저", user)
 
+                            println(answer)
+
                             find = true
                             break
                         }
@@ -102,16 +127,17 @@ class ProblemSolveScreen :AppCompatActivity() {
                     .placeholder(R.drawable.text_background)
                     .into(imageViewProblemSolve)
 
-                btn_answerSubmit.setOnClickListener {
+                btn_answerSubmit.setOnClickListener {     // 정답 제출 버튼 클릭 시
                     val input = textInputEditText.text.toString()
                     val input1 : Long = input.toLong()
                     val builder = AlertDialog.Builder(this)
+
                     builder.setTitle("정답을 제출하시겠습니까?")
                     builder.setMessage("한번 제출한 답은 변경할 수 없습니다.")
                         .setPositiveButton("제출", DialogInterface.OnClickListener { dialog, which ->
                             val builder = AlertDialog.Builder(this)
 
-                            if (answerNum == input1) {
+                            if (answer == input1) {
 
                                 builder.setMessage("정답입니다!")
                                     .setPositiveButton(
@@ -142,6 +168,7 @@ class ProblemSolveScreen :AppCompatActivity() {
                     window?.setGravity(Gravity.CENTER)
                     alertDialog.show()
                 }
+
             }
 
     }
