eca81c991403359c1087df65a2281f83c7953635
gikhoon <ghikhoon@naver.com>
2022-07-29 15:50:29 +0900

    0729 NGH UserName 만들어놓음(이름:"userName")->enter처리와 최대 길이 처리해야함
    세부과목 보이게 셋팅 완료->"공통" 부분 처리하는 법 미흡함

--- app/src/main/java/com/example/androidstudioproject/SettingFragment.kt
+++ app/src/main/java/com/example/androidstudioproject/SettingFragment.kt
@@ -2,6 +2,7 @@ package com.example.androidstudioproject
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.annotation.ArrayRes
 import androidx.preference.*
 
 
@@ -42,20 +43,16 @@ class SettingFragment : PreferenceFragmentCompat() {
             }
             else if (key.equals("subject")) {
                 mPref.edit().run {
-                    val listPreference : ListPreference?= findPreference("detailSubject")
-                    val select = mPref.getString(key, "")
-                    val grade = mPref.getString("userGradeSetting","")
-                    if(select.equals("국어")){
-                        if(grade.equals("3학년")) listPreference?.setEntries(R.array.grade3_korean)
-                        else if(grade.equals("1학년")) listPreference?.setEntries(R.array.common)
-                    }
-                    preference?.setSummary(select)
+                    remove("detailSubject")
+                    checked()
+                    preference?.setSummary(mPref.getString(key,""))
                     apply()
                 }
             }
             else if (key.equals("userGradeSetting")) {
-                checked()
                 mPref.edit().run {
+                    remove("detailSubject")
+                    checked()
                     preference?.setSummary(mPref.getString(key, ""))
                     apply()
                 }
@@ -67,19 +64,43 @@ class SettingFragment : PreferenceFragmentCompat() {
                             .toString() + "분")
                     apply()
                 }
-
+            } else if(key.equals("detailSubject")){
+                mPref.edit().run {
+                    preference?.setSummary(mPref.getString(key, ""))
+                    apply()
+                }
             }
         }
     private fun checked(){
         val detailSubjectPre : Preference? = findPreference("detailSubject")
         val grade = mPref?.getString("userGradeSetting","")
         val subject = mPref?.getString("subject","")
+        var arr : Int = R.array.common
         if(grade.equals("")||subject.equals("")){
             detailSubjectPre?.setEnabled(false)
+            detailSubjectPre?.setSummary("학년, 과목을 선택해주세요")
         }else{
             detailSubjectPre?.setEnabled(true)
             val listPreference : ListPreference?= findPreference("detailSubject")
-            //if(grade.equals("3학년"))
+            if(grade.equals("1학년")) arr=R.array.common
+            else if(grade.equals("2학년")){
+                if(subject.equals("과학탐구")) arr = R.array.grade2_science
+                else if(subject.equals("사회탐구")) arr = R.array.grade23_social
+                else arr=R.array.common
+            }
+            else if(grade.equals("3학년")){
+                if(subject.equals("과학탐구")) arr= R.array.grade3_science
+                else if(subject.equals("사회탐구")) arr=R.array.grade23_social
+                else if(subject.equals("국어")) arr =R.array.grade3_korean
+                else if(subject.equals("수학")) arr=R.array.grade3_math
+                else arr = R.array.common
+            }else{
+                detailSubjectPre?.setEnabled(false)
+                return
+            }
+            detailSubjectPre?.setSummary(mPref?.getString("detailSubject",""))
+            listPreference?.setEntries(arr)
+            listPreference?.setEntryValues(arr)
         }
     }
 }
